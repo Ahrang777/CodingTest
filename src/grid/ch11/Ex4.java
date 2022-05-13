6f42d0b95c2b33a077518ab8499cd9ed15f28bce
JunginPark4364 <wjddlsee@gmail.com>
2022-05-13 16:20:10 +0900

    ch11 주석 추가

--- src/grid/ch11/Ex4.java
+++ src/grid/ch11/Ex4.java
@@ -34,6 +34,9 @@ public class Ex4 {
         int target = 1;
         for (int i = 0; i < n; i++) {
             // 만들 수 없는 금액을 찾았을 때 반복 종료
+            //coin = coins.get(i)
+            //coin 자체로 해당 값 만들 수 있고 1~target-1을 만들 수 있으니까 coin + 1 ~ target + coin -1 까지 만들 수 있다.(이전 것들에 coin만 더한것)
+            //즉, 1 ~ target -1, coint, coin + 1 ~ target + coin -1 로 1 ~ target + coin -1 까지 만들 수 있는것
             if (target < coins.get(i)) break;
             target += coins.get(i);
         }
--- src/grid/ch11/Ex6.java
+++ src/grid/ch11/Ex6.java
@@ -2,6 +2,12 @@ package grid.ch11;
 
 import java.util.*;
 
+/*
+https://programmers.co.kr/learn/courses/30/lessons/42891
+
+food_times: [3,1,2], k: 5
+result: 1
+ */
 public class Ex6 {
 
     static class Food implements Comparable<Food>{
@@ -30,6 +36,7 @@ public class Ex6 {
 
     static class Solution {
         public int solution(int[] food_times, long k) {
+            //전체 음식 먹는 시간이 k 보다 작거나 같으면 -1
             long sum = 0;
             for(int i=0;i<food_times.length;i++){
                 sum+=food_times[i];
@@ -41,22 +48,25 @@ public class Ex6 {
                 pq.offer(new Food(food_times[i], i+1));
             }
 
-            sum = 0;
-            long prev = 0;
-            long length = food_times.length;
+            sum = 0;    //먹은 시간
+            long prev = 0;  //가장 최근에 다 먹은 음식의 원래 할당 시간
+            long length = food_times.length;    //남은 음식 개수
 
+            //sum+((pq.peek().getTime() - prev)*length) > k 이면 남은 음식들은 k분 내로 완전히 다 못먹음
             while(sum+((pq.peek().getTime() - prev)*length)<=k){
-                int now = pq.poll().getTime();
-                sum+=(now-prev)*length;
-                length--;
-                prev = now;
+                int now = pq.poll().getTime();  //음식 하나 다 먹음
+                sum+=(now-prev)*length; //음식 하나 다 먹는데 걸린 시간 반영(한바퀴 도는걸로 기준)
+                length--;   //다 먹은 음식 제외
+                prev = now; //가장 최근에 먹은 음식의 시간 설정
             }
+
 
             ArrayList<Food> result = new ArrayList<>();
             while(!pq.isEmpty()){
                 result.add(pq.poll());
             }
 
+            //다 못먹고 남은 음식들 다시 번호 순대로 정렬
             Collections.sort(result, new Comparator<Food>(){
                 @Override
                 public int compare(Food a, Food b) {
@@ -64,6 +74,7 @@ public class Ex6 {
                 }
             });
 
+            //result 리스트에 있는 음식은 다 못 먹는 음식이기에 없어질 걱정 안 하고 아래처럼 단순히 index 찾기 가능
             return result.get((int) ((k-sum)%length)).getIndex();
         }
     }
