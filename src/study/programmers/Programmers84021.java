bf56059d2d7dcc20c6115306f19fbe0fa3355d31
JunginPark4364 <wjddlsee@gmail.com>
2022-09-02 20:01:44 +0900

    Add : Add programmers(dfs/bfs)

---
+++ src/study/programmers/Programmers84021.java
@@ -0,0 +1,366 @@
+package study.programmers;
+
+import java.util.*;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/84021
+ *
+ * 퍼즐 조각 채우기
+ * 다시풀기
+ */
+public class Programmers84021 {
+
+    /*
+    int answer = 0, n = 0;
+    int[][] wh = {{1, 0, -1, 0}, {0, 1, 0, -1}}, board;
+    StringBuilder sb = new StringBuilder();
+    List<String> empty = new ArrayList<>();
+    public int solution(int[][] game_board, int[][] table) {
+        n = game_board.length;
+        board = game_board;
+        // 빈 칸을 문자열로 만들어 저장
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] == 0) {
+                    sb = new StringBuilder();
+                    dfs(i, j, 1);
+                    empty.add(sb.toString());
+                }
+            }
+        }
+        // dfs를 쓰기 위해 table의 값들을 -1
+        for(var t : table) {
+            for(int i = 0; i < n; i++) {
+                t[i]--;
+            }
+        }
+        board = table;
+
+        // table을 90도씩 돌리면서 확인해본다.
+        for(int r = 0; r < 4; r++) {
+            rotate();
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(board[i][j] != -1) {
+                        sb = new StringBuilder();
+                        dfs(i, j, board[i][j] ^ 1);
+                        // 존재한다면 퍼즐 조각 만큼 answer에 더해줌
+                        int idx = empty.indexOf(sb.toString());
+                        if(idx != -1) {
+                            empty.remove(idx);
+                            answer += sb.toString().replaceAll("[()]", "").length() + 1;
+                            dfs(i, j, -1);
+                        }
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    void rotate() {
+        int[][] rotate = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotate[i][j] = board[n - j - 1][i];
+            }
+        }
+        board = rotate;
+    }
+
+    void dfs(int a, int b, int v) {
+        board[a][b] = v;
+        sb.append("(");
+        for(int i = 0; i < 4; i++) {
+            int x = a + wh[0][i], y = b + wh[1][i];
+            if(0 <= x && 0 <= y && x < n && y < n && board[x][y] != board[a][b] && board[x][y] != -1) {
+                sb.append(i);
+                dfs(x, y, v);
+            }
+        }
+        sb.append(")");
+    }
+     */
+
+    static class Point implements Comparable<Point> {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point other) {
+            if (this.x != other.x) {
+                return this.x - other.x;
+            }
+
+            return this.y - other.y;
+        }
+    }
+
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, -1, 0, 1};
+
+    public int solution(int[][] game_board, int[][] table) {
+        int answer = -1;
+
+        int size = table.length;
+        boolean[][] gVisited = new boolean[size][size];
+        boolean[][] tVisited = new boolean[size][size];
+
+        List<List<Point>> gList = new ArrayList<>();
+        List<List<Point>> tList = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (!gVisited[i][j] && game_board[i][j] == 0) {
+                    bfs(i, j, gVisited, game_board, 0, gList);
+                }
+
+                if (!tVisited[i][j] && table[i][j] == 1) {
+                    bfs(i, j, tVisited, table, 1, tList);
+                }
+            }
+        }
+
+        answer = find(gList, tList);
+        return answer;
+    }
+
+    public int find(List<List<Point>> g, List<List<Point>> t) {
+        int cnt = 0;
+
+        int gSize = g.size();
+        int tSize = t.size();
+
+        // g 항목들에 블록 들어갔는지 여부
+        boolean[] visited = new boolean[gSize];
+
+        for (int i = 0; i < tSize; i++) {
+            List<Point> tp = t.get(i);
+            for (int j = 0; j < gSize; j++) {
+                List<Point> gp = g.get(j);
+
+                if (tp.size() == gp.size() && !visited[j]) {
+
+                    if (isRotate(gp, tp)) {
+                        visited[j] = true;
+                        cnt += tp.size();
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        return cnt;
+    }
+
+    public boolean isRotate(List<Point> g, List<Point> t) {
+        boolean isCorrect = false;
+
+        Collections.sort(g);
+
+        for (int i = 0; i < 4; i++) {
+            Collections.sort(t);
+            int x = t.get(0).x;
+            int y = t.get(0).y;
+
+            // 다시 0, 0 기준으로 변경
+            for (Point p : t) {
+                p.x -= x;
+                p.y -= y;
+            }
+
+            boolean isCorrectPoint = true;
+            for (int j = 0; j < g.size(); j++) {
+                Point gp = g.get(j);
+                Point tp = t.get(j);
+
+                if (gp.x != tp.x || gp.y != tp.y) {
+                    isCorrectPoint = false;
+                    break;
+                }
+            }
+
+            if (isCorrectPoint) {
+                isCorrect = true;
+                break;
+            } else { // 90도 회전
+                for (Point tp : t) {
+                    int tmp = tp.x;
+                    tp.x = -tp.y;
+                    tp.y = tmp;
+                }
+            }
+        }
+
+        return isCorrect;
+    }
+
+    public void bfs(int x, int y, boolean[][] visited, int[][] graph, int emptyOrBlock, List<List<Point>> list) {
+        Queue<Point> q = new LinkedList<>();
+        visited[x][y] = true;
+        q.offer(new Point(x, y));
+        List<Point> sub = new ArrayList<>();
+        sub.add(new Point(x-x, y-y));
+
+        while(!q.isEmpty()) {
+            Point now = q.poll();
+            int curX = now.x;
+            int curY = now.y;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = curX + dx[i];
+                int ny = curY + dy[i];
+
+                if (nx < 0 || nx >= graph.length || ny < 0 || ny >= graph.length)   continue;
+
+                if (!visited[nx][ny] && graph[nx][ny] == emptyOrBlock) {
+                    visited[nx][ny] = true;
+                    q.offer(new Point(nx, ny));
+                    sub.add(new Point(nx - x, ny - y));
+                }
+            }
+        }
+
+        list.add(sub);
+    }
+
+    /*
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+
+    public int solution(int[][] game_board, int[][] table) {
+        int answer = -1;
+
+        boolean[][] visitedTable = new boolean[table.length][table.length];
+        boolean[][] visitedBoard = new boolean[game_board.length][game_board.length];
+        List<List<int[]>> boardList = new ArrayList<>();
+        List<List<int[]>> tableList = new ArrayList<>();
+
+        for (int i = 0; i < table.length; i++){
+            for (int j = 0; j < table.length; j++){
+
+                if (table[i][j] == 1 && !visitedTable[i][j]){
+                    bfs(i, j, visitedTable, table, 1, tableList);
+                }
+
+                if (game_board[i][j] == 0 && !visitedBoard[i][j]){
+                    bfs(i, j, visitedBoard, game_board, 0, boardList);
+                }
+            }
+        }
+
+        answer = findBlock(boardList, tableList);
+
+        return answer;
+    }
+
+    public int findBlock(List<List<int[]>> board, List<List<int[]>> table){
+        int size = 0;
+        int tableLen = table.size();
+        int boardLen = board.size();
+        boolean[] visitedBoard = new boolean[boardLen];
+        for (int i = 0; i < tableLen; i++){
+            List<int[]> tablePoints = table.get(i);
+            for (int j = 0; j < boardLen; j++){
+                List<int[]> boardPoints = board.get(j);
+
+                if (tablePoints.size() == boardPoints.size() && !visitedBoard[j]){ //좌표 개수 같을때
+                    if(isRotate(boardPoints, tablePoints)){ //회전해서 맞는지 확인
+                        size += tablePoints.size();
+                        visitedBoard[j] = true;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        return size;
+    }
+
+    public boolean isRotate(List<int[]> board, List<int[]> table){
+        boolean isCollect = false;
+
+        board.sort((o1, o2) -> {
+            return o1[0] > o2[0]?1 : o1[0] < o2[0]?-1 : Integer.compare(o1[1], o2[1]);
+        });
+
+        for (int i = 0; i < 4; i++){ //table퍼즐 0, 90, 180, 270도 회전
+
+            table.sort((o1, o2) -> {
+                return o1[0] > o2[0]?1 : o1[0] < o2[0]?-1 : Integer.compare(o1[1], o2[1]);
+            });
+            int nearZeroX = table.get(0)[0];
+            int nearZeroY = table.get(0)[1];
+
+            for (int j = 0; j < table.size(); j++){
+                table.get(j)[0] -= nearZeroX;
+                table.get(j)[1] -= nearZeroY;
+            }
+
+
+            boolean isCollectPoint = true;
+            for (int j = 0; j < board.size(); j++){ //좌표 비교
+                int[] boardPoint = board.get(j);
+                int[] tablePoint = table.get(j);
+
+                if (boardPoint[0] != tablePoint[0] || boardPoint[1] != tablePoint[1]){
+                    isCollectPoint = false;
+                    break;
+                }
+            }
+
+            if (isCollectPoint){
+                isCollect = true;
+                break;
+            } else{ //90도 회전 : x,y -> y, -x
+                for(int j = 0; j < table.size(); j++){
+                    int temp = table.get(j)[0];
+                    table.get(j)[0] = table.get(j)[1];
+                    table.get(j)[1] = -temp;
+                }
+            }
+        }
+        return isCollect;
+
+    }
+
+
+
+    public void bfs(int currentX, int currentY, boolean[][] visited, int[][] graph,
+                      int blockOrEmpty, List<List<int[]>> list){
+
+        Queue<int[]> queue = new LinkedList<>();
+        visited[currentX][currentY] = true;
+        queue.add(new int[]{currentX, currentY});
+        List<int[]> subList = new ArrayList<>();
+        subList.add(new int[]{currentX-currentX, currentY-currentY});
+
+        while (!queue.isEmpty()){
+            int[] pop = queue.remove();
+            int popX = pop[0];
+            int popY = pop[1];
+
+            for (int i = 0; i < 4; i++){
+                int nextX = popX + dx[i];
+                int nextY = popY + dy[i];
+                if (nextX < 0 || nextX >= graph.length || nextY < 0 || nextY >= graph.length){
+                    continue;
+                }
+                if (!visited[nextX][nextY] && graph[nextX][nextY] == blockOrEmpty){
+
+                    visited[nextX][nextY] = true;
+                    queue.add(new int[]{nextX, nextY});
+                    subList.add(new int[]{nextX-currentX, nextY-currentY});
+                }
+            }
+        }
+        list.add(subList);
+    }
+     */
+}
--- src/study/programmers/Programmers87694.java
+++ src/study/programmers/Programmers87694.java
@@ -1,14 +1,158 @@
 package study.programmers;
 
+import java.util.*;
+
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/87694
  *
  * 아이템 줍기
+ * 다시풀기
+ * 참고: https://loosie.tistory.com/544
  */
 public class Programmers87694 {
 
+    static class Rectangle {
+        int x1, y1, x2, y2;
+
+        public Rectangle (int x1, int y1, int x2, int y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+    }
+
+    static List<Rectangle> list = new ArrayList<>();
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+
+    /**
+     *
+     * 1 1
+     * 1 1
+     * 이 경우 ㄷ 모양인지 ㅁ 인지 구분이 안된다.
+     *
+     * 때문에 map 의 범위를 2배를 하면
+     * 1 1 1
+     * 1 0 0
+     * 1 1 1
+     * 처럼 ㄷ 자 모양인지 ㅁ 모양인지 구분이 된다.
+     *
+     * 때문에 좌표들을 2배를 늘리고 좌표가 2배씩 늘어났기때문에
+     * 마지막에 이동한 칸수는 원래 이동한 갯수보다 2배 늘어났을거라서 / 2 를 해준다.
+     */
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
+
+        int[][] map = new int[102][102];
+
+        for (int[] r : rectangle) {
+            int sx = r[0] * 2;
+            int sy = r[1] * 2;
+            int ex = r[2] * 2;
+            int ey = r[3] * 2;
+
+            list.add(new Rectangle(sx, sy, ex, ey));
+
+            for (int i = sy; i <= ey; i++) {
+                for (int j = sx; j <= ex; j++) {
+                    map[i][j] = 1;
+                }
+            }
+        }
+
+        answer = bfs(map, characterX*2, characterY*2, itemX*2, itemY*2);
+
         return answer;
     }
+
+    public int bfs(int[][] map, int x, int y, int tx, int ty) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y, 1});
+        map[y][x] = -1;
+
+        while (!q.isEmpty()){
+            int[] now = q.poll();
+            int curX = now[0];
+            int curY = now[1];
+            int move = now[2];
+
+            if (curX == tx && curY == ty) {
+                return move / 2;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = curX + dx[i];
+                int ny = curY + dy[i];
+
+                if (map[ny][nx] == 1 && isBoundary(nx, ny)) {
+                    map[ny][nx] = -1;
+                    q.add(new int[]{nx, ny, move + 1});
+                }
+            }
+
+        }
+
+        return -1;
+    }
+
+    // 모든 사각형들에 대해서 하나의 사각형이라도 내부에 존재하게 되면 해당 좌표는 경계가 아님
+    public boolean isBoundary(int x, int y) {
+        for (Rectangle r : list) {
+            if (r.x1 < x && x < r.x2 && r.y1 < y && y < r.y2)   return false;
+        }
+
+        return true;
+    }
+
+    /**
+     *   LUX  RUX
+     *     L  R
+     * LUX UL UL RUX
+     * L  LDX RDX R
+     * LDX D  D  RDX
+     */
+    /*
+    static String[][] shape;
+    static int startX, startY, targetX, targetY, answer, total;
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        shape = new String[52][52];
+        startX = characterX; startY = characterY; targetX = itemX; targetY = itemY;
+        answer = total = 0;
+
+        for(int i=0; i<52; i++) Arrays.fill(shape[i],""); // ""로 초기화
+
+        for(int[] xy : rectangle){
+            int leftX = xy[0], rightX = xy[2], leftY = xy[1], rightY = xy[3];
+
+            // 꼭지점 (왼쪽아래(LDX), 오른쪽아래(RDX), 왼쪽위(LUX), 오른쪽위(RUX))
+            shape[leftX][leftY] = "LDX"; shape[rightX][leftY] = "RDX"; shape[leftX][rightY] = "LUX"; shape[rightX][rightY] = "RUX";
+
+            for(int x=leftX+1; x<rightX; x++){// 상(U), 하(D)
+                shape[x][rightY] += "U"; shape[x][leftY] += "D";
+            }
+
+            for(int y=leftY+1; y<rightY; y++){// 좌(L), 우(R)
+                shape[leftX][y] += "L"; shape[rightX][y] += "R";
+            }
+        }
+
+        followLine(characterX, characterY);
+
+        return Math.min(answer, total-answer);
+    }
+
+    public void followLine(int x, int y){
+        String location = shape[x][y];
+        if(location.equals("RU") || location.equals("UR") || location.equals("LUX") || location.equals("U"))  x++;
+        if(location.equals("LD") || location.equals("DL") || location.equals("RDX") || location.equals("D"))  x--;
+        if(location.equals("LU") || location.equals("UL") || location.equals("LDX") || location.equals("L"))  y++;
+        if(location.equals("RD") || location.equals("DR") || location.equals("RUX") || location.equals("R"))  y--;
+        total++;
+        if(x == targetX && y == targetY)    answer = total;
+        if(x == startX && y == startY)      return;
+        followLine(x, y);
+    }
+     */
 }
