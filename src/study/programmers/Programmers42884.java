7dc1e7054c835f6bd601196bd240c58a175a80b2
JunginPark4364 <wjddlsee@gmail.com>
2022-09-12 15:53:06 +0900

    Add : Add programmers(greedy)

--- src/study/programmers/Programmers42884.java
+++ src/study/programmers/Programmers42884.java
@@ -1,5 +1,7 @@
 package study.programmers;
 
+import java.util.Arrays;
+
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/42884
  *
@@ -10,6 +12,17 @@ public class Programmers42884 {
 
     public static int solution(int[][] routes) {
         int answer = 0;
+
+        Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]);
+
+        int cam = Integer.MIN_VALUE;
+        for (int[] route : routes) {
+            if (cam < route[0]) {
+                cam = route[1];
+                answer++;
+            }
+        }
+
         return answer;
     }
 
