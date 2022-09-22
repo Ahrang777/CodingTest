ab868bb569a3b2249e65db7f8e2fbb8f5875bea2
JunginPark4364 <wjddlsee@gmail.com>
2022-09-22 17:27:52 +0900

    Add : Add programmers(binary search)

---
+++ src/study/programmers/Programmers43236.java
@@ -0,0 +1,59 @@
+package study.programmers;
+
+import java.util.*;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/43236
+ *
+ * 징검다리
+ * 
+ * 다시풀기
+ */
+public class Programmers43236 {
+
+    public static int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+
+        Arrays.sort(rocks);
+        int left = 0;
+        int right = distance;
+
+        while (left <= right) {
+            // 사이거리 최솟값
+            int mid = (left + right) / 2;
+            int prev = 0;   // 거리 측정 시작점
+            int remove = 0; // 제거해야 하는 바위
+
+            for (int i = 0; i < rocks.length; i++) {
+                // 사이거리가 최솟값으로 정한 mid보다 작은경우 해당 바위 제거
+                if (rocks[i] - prev < mid) {
+                    remove++;
+                    if (remove > n) break;
+                } else { // 사이거리가 mid보다 크면 시작점 갱신
+                    prev = rocks[i];
+                }
+            }
+
+            // 최솟값 mid를 줄여야함
+            if (remove > n) {
+                right = mid - 1;
+            } else {
+                // n보다 적거나 같게 바위를 지우는 경우 현재 mid보다 더 큰 값이 존재할 수 있기에 확인해야함
+                // mid는 현재 최솟값이기에 최솟값중 가장 큰 answer를 구하기 위해서는 크기비교가 필요
+                left = mid + 1;
+                answer = answer > mid ? answer : mid;
+            }
+        }
+        
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int distance = 25;
+        int[] rocks = {2, 14, 11, 21, 17};
+        int n = 2;
+
+        System.out.println(solution(distance, rocks, n));
+
+    }
+}
---
+++ src/study/programmers/Programmers43238.java
@@ -0,0 +1,41 @@
+package study.programmers;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/43238
+ *
+ * 입국심사
+ */
+public class Programmers43238 {
+
+    public static long solution(int n, int[] times) {
+        long answer = 0;
+
+        long start = 1;
+        long end = (long) 1000000000 * n;
+
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long total = 0; // 총 심사한 인원
+
+            for (int time : times) {
+                total += mid / time;
+            }
+
+            if (total < n) { // 전체 인원보다 심사한 인원이 적은 경우 >> 시간이 더 필요
+                start = mid + 1;
+            } else { // 전체 인원보다 심사한 인원이 많은 경우 >> 시간을 더 줄여서 최솟값 찾기
+                end = mid - 1;
+                answer = mid;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[] times = {7, 10};
+
+        System.out.println(solution(n, times));
+    }
+}
