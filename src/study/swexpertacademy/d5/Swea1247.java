c7d8f51a924377b927e3a1d7a909fdcf7cd9fe97
JunginPark4364 <wjddlsee@gmail.com>
2023-07-23 13:47:54 +0900

    feat: SWEA 문제 다른 풀이

--- src/study/swexpertacademy/d5/Swea1247.java
+++ src/study/swexpertacademy/d5/Swea1247.java
@@ -2,6 +2,7 @@ package study.swexpertacademy.d5;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -9,7 +10,81 @@ import java.util.StringTokenizer;
  * 최적 경로
  */
 public class Swea1247 {
-	static class Position {
+	static Point[] points;
+	static int[][] dp;
+	static int N;
+	static int INF = Integer.MAX_VALUE;
+
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T= Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		for(int test_case = 1; test_case <= T; test_case++){
+			sb.append("#").append(test_case).append(" ");
+
+			N = Integer.parseInt(br.readLine());
+			points = new Point[N+2];
+			dp = new int[N+1][(1<<(N+1))-1];
+
+			for(int i=0; i<N+1; i++){
+				Arrays.fill(dp[i], INF);}
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			Point start = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			Point end = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			points[0] = start;
+			for(int i=1; i<=N; i++){
+				points[i] = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			}
+			points[N+1] = end;
+
+			sb.append(go(0, 1)).append("\n");
+		}
+
+		br.close();
+		System.out.print(sb);
+	}
+
+	static int go(int now, int visit){
+
+		if(visit == (1<<(N+1))-1){
+			return getDistance(now, N+1);
+		}
+
+		if(dp[now][visit] != INF){
+			return dp[now][visit];
+		}
+
+		for(int i=0; i<N+1; i++){
+			if((visit&(1<<i))!=0){
+				continue;
+			}
+
+			dp[now][visit] = Math.min(dp[now][visit], go(i, visit|(1<<i))+getDistance(now, i));
+		}
+
+		return dp[now][visit];
+	}
+
+	static int getDistance(int p1, int p2){
+		return Math.abs(points[p1].x-points[p2].x)+Math.abs(points[p1].y-points[p2].y);
+	}
+
+	static class Point{
+		int x;
+		int y;
+
+		Point(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}	// DP, 비트마스킹 이용
+
+
+	/*static class Position {
 		int x, y;
 
 		public Position(int x, int y) {
@@ -77,5 +152,5 @@ public class Swea1247 {
 				visited[i] = false;
 			}
 		}
-	}
+	}*/
 }
--- src/study/swexpertacademy/d5/Swea1248.java
+++ src/study/swexpertacademy/d5/Swea1248.java
@@ -2,8 +2,6 @@ package study.swexpertacademy.d5;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 /**
@@ -11,7 +9,99 @@ import java.util.StringTokenizer;
  * 공통조상
  */
 public class Swea1248 {
-	static int V, E, A, B;
+	static class Node {
+		int parent, left, right;
+
+	}
+	static int V, E, N1, N2;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+
+			st = new StringTokenizer(br.readLine());
+
+			V = Integer.parseInt(st.nextToken());
+			E = Integer.parseInt(st.nextToken());
+			N1 = Integer.parseInt(st.nextToken());
+			N2 = Integer.parseInt(st.nextToken());
+
+			Node[] nodes = new Node[V + 1];
+			for (int i = 0; i < V + 1; i++) {
+				nodes[i] = new Node();
+			}
+
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < E; i++) {
+				int p = Integer.parseInt(st.nextToken());	// 부모
+				int c = Integer.parseInt(st.nextToken());	// 자식
+
+				if (nodes[p].left == 0) {
+					nodes[p].left = c;
+				} else {
+					nodes[p].right = c;
+				}
+
+				nodes[c].parent = p;
+			}
+
+			boolean[] visited = new boolean[V + 1];
+
+			int root = 1;	// 공통 루트노드 >> 적어도 최상단 노드인 1은 공통 부모이다.
+			int n1 = N1;
+			int n2 = N2;
+
+			while (true) {	// 못해도 n1, n2가 최상단 루트인 1에서는 만나기에 반복문 탈출 가능
+				if (n1 != 1) {
+					int parent = nodes[n1].parent;
+					if (visited[parent]) {	// 다른 정점의 부모 노드들 중 가장 먼저 만났기에 가장 가까운 공통 부모
+						root = parent;
+						break;
+					}
+					visited[parent] = true;
+					n1 = parent;
+				}
+				if (n2 != 1) {
+					int parent = nodes[n2].parent;
+					if (visited[parent]) {
+						root = parent;
+						break;
+					}
+					visited[parent] = true;
+					n2 = parent;
+				}
+			}
+			
+			int size = dfs(nodes, root);
+
+			sb.append("#").append(tc)
+				.append(" ").append(root)
+				.append(" ").append(size)
+				.append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	private static int dfs(Node[] nodes, int index) {
+		int result = 1;	// 말단 노드의 경우 자신을 카운트하기 위해 1
+		if (nodes[index].left != 0) {	// 왼쪽 자식이 있는 경우
+			// 왼쪽 파고들기
+			// 왼쪽 부분 모아서 반영
+			result += dfs(nodes, nodes[index].left);
+		}
+		if (nodes[index].right != 0) {	// 오른쪽 자식이 있는 경우
+			result += dfs(nodes, nodes[index].right);
+		}
+		return result;
+	}
+
+
+	/*static int V, E, A, B;
 	static int[] parents;
 
 	public static void main(String[] args) throws Exception {
@@ -39,13 +129,18 @@ public class Swea1248 {
 				parents[c] = p;
 			}
 
+			// A정점의 조상 가까운 순으로 저장
 			List<Integer> aList = new ArrayList<>();
+			
+			// B정점의 조상 가까운 순으로 저장
 			List<Integer> bList = new ArrayList<>();
 
 			addParents(A, aList);
 			addParents(B, bList);
 
-			int target = 1;
+			// 적어도 루트는 동일하기에 최상단 루트인 1을 기본으로 설정
+			// 가장 가까운 루트 정점
+			int target = 1;	
 			for (int n : aList) {
 				if (bList.contains(n)) {
 					target = n;
@@ -53,8 +148,10 @@ public class Swea1248 {
 				}
 			}
 
+			// 가장 가까운 공통정점에서 최상단 루트와의 연결 끊기
 			parents[target] = target;
 
+			// 각 루트 노드 갱신
 			for (int i = 1; i <= V; i++) {
 				findRoot(i);
 			}
@@ -87,13 +184,5 @@ public class Swea1248 {
 		}
 
 		return parents[x] = findRoot(parents[x]);
-	}
-
-	static void union(int a, int b) {
-		a = findRoot(a);
-		b = findRoot(b);
-
-		if (a < b)	parents[b] = a;
-		else parents[a] = b;
-	}
+	}*/
 }
